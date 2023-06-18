d42e68432c81628c8e65fa05f6f1d4e127880bfb
Biswapriyo Nath <nathbappai@gmail.com>
2023-06-18 18:18:12 +0530

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+# adbwinapi
+
+This repository is to provide the shared libraries required for the
+[android-tools](https://packages.msys2.org/base/mingw-w64-android-tools) package
+in MINGW-packages.
+
+## License
+
+This repository is licensed with the same [Apache-2.0 license](LICENSE) used in
+AOSP [platform/development](https://android.googlesource.com/platform/development.git)
+repository.
---
+++ build.cmd
@@ -0,0 +1,37 @@
+:: Build script for adbwinapi
+
+set "GIT_TAG=33.0.3"
+set "GIT_DIR=development"
+set "BUILD_ARCH=%~1"
+
+if not exist "%cd%\%GIT_DIR%\" (
+git clone ^
+--depth=1 ^
+--branch platform-tools-"%GIT_TAG%" ^
+https://android.googlesource.com/platform/development.git ^
+"%GIT_DIR%"
+
+git apply ^
+--directory="%GIT_DIR%" ^
+0001-adbwinusb-fix-build.patch
+)
+
+if "%BUILD_ARCH%" == "" (
+set "BUILD_ARCH=x64"
+)
+
+copy CMakeLists.txt "%GIT_DIR%\CMakeLists.txt"
+
+cmake ^
+-G "Visual Studio 17 2022" ^
+-A "%BUILD_ARCH%" ^
+-B "build-%BUILD_ARCH%" ^
+-S "%GIT_DIR%"
+
+cmake ^
+--build "build-%BUILD_ARCH%" ^
+--config Release
+
+7z a ^
+-tzip "adbwinapi_sources.zip" ^
+"%GIT_DIR%\host\windows\usb"
